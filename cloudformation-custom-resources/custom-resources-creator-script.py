c61e83422288cedbf6f32fa2299b7bb696ac703d
Tomasz Stachlewski <public@stachlewski.org>
2019-10-26 08:30:10 +0200

    script modification

--- cloudformation-custom-resources/custom-resources-creator-script.py
+++ cloudformation-custom-resources/custom-resources-creator-script.py
@@ -5,14 +5,20 @@
 
 import boto3
 
-bucket_prefix = "waf-dashboards-"
+bucket_prefix = "aws-waf-dashboard-"
 regions = ["us-east-1","us-east-2","us-west-1","us-west-2","ca-central-1","eu-central-1","eu-west-1","eu-west-2","eu-west-3","eu-north-1","ap-northeast-1","ap-northeast-2","ap-northeast-3","ap-southeast-1","ap-southeast-2","ap-south-1","sa-east-1"]
 
 for region in regions:
+    
     print("Working on region: " + region);
     bucket_name = bucket_prefix + region
+    
     #s3 = boto3.client('s3', region_name=region)
-    #s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': region})
+    #if (region != 'us-east-1'):
+    #    s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={'LocationConstraint': region})
+    #else:
+    #    s3.create_bucket(Bucket=bucket_name)
+    
 
     file1 = 'domain-setter-lambda.zip'
     file2 = 'es-cognito-auth-lambda.zip'
@@ -32,4 +38,7 @@ for region in regions:
 #Delete all local resources which are not needed anymore
 #rm domain-setter-lambda.zip
 #rm es-cognito-auth-lambda.zip
-#rm kibana-customizer-lambda.zip
+#rm kibana-customizer-lambda.zip#Zip all Lambda functions
+#cd domain-setter-lambda; zip -r ../domain-setter-lambda.zip *; cd ..
+#cd es-cognito-auth-lambda; zip -r ../es-cognito-auth-lambda.zip *; cd ..
+#cd kibana-customizer-lambda; zip -r ../kibana-customizer-lambda.zip *; cd ..
